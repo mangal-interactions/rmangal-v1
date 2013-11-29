656edd80767857be946c09eaaa2df11f5c7e2569
Tim Poisot <timothee.poisot@uqar.ca>
2013-11-28 22:18:56 -0500

    Function to get a specific taxa

--- R/list-pop.R
+++ R/list-pop.R
@@ -8,6 +8,10 @@
 #' @param taxa A \code{taxa} object in \code{list} format
 listPop <- function(API, taxa)
 {
+  if(class(API)!="mangal") stop("The API argument must be a valid mangalAPI object")
+  if(!is.list(taxa)) stop("The taxa argument must be a list")
+  if(is.null(taxa$id)) stop("The taxa object must have a id attribute")
+  if(!is.character(taxa$id)) stop("The id attribute of taxa must be a character")
   list_pop <- httr::GET(paste(API$url,'taxa',taxa$id,'pop',sep='/'))
   if(list_pop$status_code == 200) return(content(list_pop))
 }
--- R/list-taxa.R
+++ R/list-taxa.R
@@ -2,11 +2,12 @@
 #'
 #' Makes a list of all available taxa in the queried database.
 #'
-#'  @export
+#' @export
 #'
 #' @param API An object of class \code{\link{mangalAPI}}
 listTaxa <- function(API)
 {
+  if(class(API)!="mangal") stop("The API argument must be a valid mangalAPI object")
   list_taxa <- httr::GET(paste(API$url,'taxa',sep='/'))
   if(list_taxa$status_code == 200) return(content(list_taxa))
 }
