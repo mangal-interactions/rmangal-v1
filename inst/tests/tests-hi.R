709d7d32077ca2e45880416d007917967e403136
Tim Poisot <timothee.poisot@uqar.ca>
2013-11-28 19:34:12 -0500

    Additional test and travis.yml file

--- inst/tests/tests-hi.R
+++ inst/tests/tests-hi.R
@@ -3,4 +3,5 @@ context("Test of the first contact with the API")
 test_that("A default call is a success",{
   default <- mangalAPI()
   expect_equal(sayHi(default)$method, "GET")
+  expect_equal(sayHi(default)$api, "0.9a")
 })
