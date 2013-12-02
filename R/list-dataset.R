3344f30abd4c36677fbb272e90ba62c39946d02d
Tim Poisot <timothee.poisot@uqar.ca>
2013-12-02 14:08:08 -0500

    Fixed wrong variable name in listDataset()

--- R/list-dataset.R
+++ R/list-dataset.R
@@ -9,5 +9,5 @@ listDatasets <- function(API)
 {
   if(class(API)!="mangal") stop("The API argument must be a valid mangalAPI object")
   list_ds <- httr::GET(paste(API$url,'dataset', sep='/'))
-  if(list_pop$status_code == 200) return(content(list_ds))
+  if(list_ds$status_code == 200) return(content(list_ds))
 }
