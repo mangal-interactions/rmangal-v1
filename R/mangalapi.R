2f1c118fc851a6122b6b01975dfc4208103fc4b6
Tim Poisot <timothee.poisot@uqar.ca>
2013-12-09 16:25:12 -0500

    Taxa + authenticate

--- R/mangalapi.R
+++ R/mangalapi.R
@@ -4,13 +4,16 @@
 #'
 #' @param url The URL to the server
 #' @param v The API version
-mangalapi <- function(url = "http://localhost:8000", v = 'v1', ...)
+#' @param user The username
+#' @param pwd The password
+mangalapi <- function(url = "http://localhost:8000", v = 'v1', user = NULL, pwd = NULL, ...)
 {
 	# ... will be updated for authentication in a future release
 	queryset <- httr::GET(paste(url, 'api', v, sep='/'))
 	if(http_status(queryset)$category == "success")
 	{
 		methods <- list()
+      if(!(is.null(user) & is.null(pwd))) methods$auth <- authenticate(user, pwd, 'basic')
 		methods$base <- url
 		list_of_methods <- content(queryset)
 		for(i in c(1:length(list_of_methods)))
