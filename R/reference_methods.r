f57365d43159caec94db615f3abce220bb8c960e
Tim Poisot <timothee.poisot@uqar.ca>
2014-01-03 15:35:09 -0500

    Added methods for references

---
+++ R/reference_methods.r
@@ -0,0 +1,36 @@
+#' @title List the references
+#'
+#' @description Returns a list of references
+#'
+#' @param api a \code{\link{mangalapi}} object
+listReference <- function(api) mangalList(api, 'reference')
+
+#' @title Get a reference
+#' 
+#' @description Returns a reference
+#'
+#' @param api a \code{\link{mangalapi}} object
+#' @param id the identifier of the reference
+getReference <- function(api, id) mangalGet(api, 'reference', id)
+
+#' @title Add a new reference
+#' 
+#' @description Post a new reference to the database
+#' 
+#' @details
+#' Requires authentication
+#' 
+#' @param api a \code{\link{mangalapi}} object
+#' @param data the reference in list format
+addReference <- function(api, data) mangalPost(api, 'reference', data)
+
+#' @title Patch a reference
+#' 
+#' @description Modify the informations for a reference
+#' 
+#' @details
+#' Requires authentication
+#' 
+#' @param api a \code{\link{mangalapi}} object
+#' @param data the reference in list format
+patchPopulation <- function(api, data) mangalPatch(api, 'reference', data)
