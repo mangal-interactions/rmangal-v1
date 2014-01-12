b367f264b29981ed40944ef0b93d673bdb2f7fff
Tim Poisot <timothee.poisot@uqar.ca>
2014-01-12 18:14:31 -0500

    Add a filtering argument in list

--- R/mangalList.R
+++ R/mangalList.R
@@ -4,8 +4,9 @@
 #'
 #' @param api a \code{\link{mangalapi}} object
 #' @param type a type of object
-mangalList <- function(api, type)
+mangalList <- function(api, type, filtering=NULL)
 {
 	if(is.null(api[[type]]) | !("get" %in% api[[type]]$verbs)) stop(paste("This API do not implement the listing of ",type,sep=''))
-	return(pagerResources(api, httr::GET(api[[type]]$url)))
+   query_url <- ifelse(is.null(filtering), api[[type]]$url, paste(api[[type]]$url,'&',filtering,sep=''))
+	return(pagerResources(api, httr::GET(query_url)))
 }
