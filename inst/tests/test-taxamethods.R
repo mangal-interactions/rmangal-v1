2e737b00cdbc2c4a5b8027b9d41d9ceb3a24d6cf
Timothée Poisot <t.poisot@gmail.com>
2014-04-01 12:13:25 -0400

    Added a few tests (default API home, taxa) See #13

---
+++ inst/tests/test-taxamethods.R
@@ -0,0 +1,10 @@
+context("Test of the taxa methods")
+
+test_that("listTaxa abd getTaxa work as they should",{
+          api <- mangalapi()
+          list_taxa <- listTaxa(api)
+          expect_is(list_taxa, "list")
+          expect_is(list_taxa[[1]], "list")
+          taxa_1 <- getTaxa(api, list_taxa[[1]]$id)
+          expect_equal(list_taxa[[1]]$name, taxa_1$name)
+})
