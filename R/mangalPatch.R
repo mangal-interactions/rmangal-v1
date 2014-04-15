045780f0bee4e981552d07a1571c695d2c1f5ee6
Timothée Poisot <t.poisot@gmail.com>
2014-04-15 17:43:36 -0400

    Added auto_unbox to toJSON
    Now I'll have to make sure that any item with (possibly) more than one
    element is passed as a list, and not as an array. This pisses me off to
    no end. Seriously.

--- R/mangalPatch.R
+++ R/mangalPatch.R
@@ -14,7 +14,7 @@ mangalPatch <- function(api, type, data)
 	if(!type == 'user') data$owner <- api$me
 	qURL <- paste(api[[type]]$url, data$id, sep='')
 	if(!(str_sub(qURL,-1)=='/')) qURL <- paste(qURL,'/',sep='')
-	queryset <- PATCH(str_c(qURL, '?', api$auth), body = toJSON(data), add_headers("Content-type" = "application/json"), api$auth)
+	queryset <- PATCH(str_c(qURL, '?', api$auth), body = toJSON(data, auto_unbox=TRUE), add_headers("Content-type" = "application/json"), api$auth)
 	if(http_status(queryset)$category == "success")
 	{
 		new_entry <- content(queryset)
--- R/mangalPost.R
+++ R/mangalPost.R
@@ -11,7 +11,7 @@ mangalPost <- function(api, type, data)
   if(is.null(data)) stop("Please provide data to add to the database")
   if(is.null(api[[type]]) | !("post" %in% api[[type]]$verbs)) stop(paste("This API do not permit POSTing objects of type ",type,sep=''))
 	if(!type == 'user') data$owner <- api$me
-	queryset <- POST(str_c(api[[type]]$url, '?', api$auth), body = toJSON(data), add_headers("Content-type" = "application/json"))
+	queryset <- POST(str_c(api[[type]]$url, '?', api$auth), body = toJSON(data, auto_unbox=TRUE), add_headers("Content-type" = "application/json"))
 	if(http_status(queryset)$category == "success")
 	{
 		new_entry <- content(queryset)
