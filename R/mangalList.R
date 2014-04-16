164f08b243db77a6b349f0cf0b4adbd1fbf34c3d
Timothée Poisot <t.poisot@gmail.com>
2014-04-16 10:40:53 -0400

    Documentation for mangalList
    Added an entry in the parameters for `filtering`.

--- R/mangalList.R
+++ R/mangalList.R
@@ -4,6 +4,7 @@
 #'
 #' @param api a \code{\link{mangalapi}} object
 #' @param type a type of object
+#' @param filtering filters to restrict the returned objects
 mangalList <- function(api, type, filtering=NULL)
 {
 	if(is.null(api[[type]]) | !("get" %in% api[[type]]$verbs)) stop(paste("This API do not implement the listing of ",type,sep=''))
