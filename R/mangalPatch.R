ea9bef2f2842d12cf4f0aa2469ca9780040fc389
Timothée Poisot <t.poisot@gmail.com>
2014-04-30 09:25:31 -0400

    Owner field is not updated when resources are patched

--- R/mangalPatch.R
+++ R/mangalPatch.R
@@ -11,11 +11,11 @@ mangalPatch <- function(api, type, data)
 	if(is.null(data)) stop("Please provide data to patch the database")
 	if(is.null(data$id)) stop("The ID field must be present to patch")
 	if(is.null(api[[type]]) | !("patch" %in% api[[type]]$verbs)) stop(str_c("This API do not permit PATCHing objects of type ",type))
-	if(!type == 'user') data$owner <- api$me
+	if(!type == 'user') data$owner <- uri_from_uname(api, data$owner)
 	qURL <- str_c(api[[type]]$url, data$id)
 	if(!(str_sub(qURL,-1)=='/')) qURL <- str_c(qURL,'/',sep='')
-  # We need to get rid of everything NULL, or else jsonlite will screw it up
-  data[names(data)[laply(data, length)==0]] <- NULL
+   # We need to get rid of everything NULL, or else jsonlite will screw it up
+   data[names(data)[laply(data, length)==0]] <- NULL
 	queryset <- PATCH(str_c(qURL, '?', api$auth), body = toJSON(data, auto_unbox=TRUE), add_headers("Content-type" = "application/json"))
 	if(http_status(queryset)$category == "success")
 	{
@@ -25,4 +25,19 @@ mangalPatch <- function(api, type, data)
 		print(content(queryset))
 		stop(http_status(queryset)$message)
 	}
+}
+
+#' @title Get the URI of a user from its username
+#'
+#' @param api a \code{\link{mangalapi}} object
+#' @param username a username
+uri_from_uname <- function(api, username)
+{
+   filter <- str_c('username__exact=', username)
+   matches <- mangalList(api, 'user', filtering=filter)
+   if (length(matches) == 0)
+   {
+      stop(str_c(username, " is not a valid username"))
+   }
+   return(resToURI(api, matches[[1]], 'user'))
 }
