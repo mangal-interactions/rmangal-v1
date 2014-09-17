99051fbb42af0da4b22230a32c3e4f041e533b42
Timothée Poisot <t.poisot@gmail.com>
2014-09-17 21:36:34 +1200

    Good parameters are passed

--- R/mangalPost.R
+++ R/mangalPost.R
@@ -11,7 +11,7 @@ mangalPost <- function(api, type, data)
   if(is.null(data)) stop("Please provide data to add to the database")
   if(is.null(api[[type]]) | !("post" %in% api[[type]]$verbs)) stop(stringr::str_c("This API do not permit POSTing objects of type ",type))
 	if(!type == 'user') data$owner <- api$me
-	queryset <- httr::POST(stringr::str_c(api[[type]]$url, '?', api$auth), body = jsonlite::toJSON(data, auto_unbox=TRUE), httr::add_headers("Content-type" = "application/json"))
+	queryset <- httr::POST(stringr::str_c(api[[type]]$url, render_parameters(api$args)), body = jsonlite::toJSON(data, auto_unbox=TRUE), httr::add_headers("Content-type" = "application/json"))
 	if(httr::http_status(queryset)$category == "success")
 	{
 		new_entry <- httr::content(queryset)
