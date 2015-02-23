6652d26f509ca4f36c98d956f6eebf28dc36ef2f
Timothée Poisot <tim@poisotlab.io>
2015-02-23 15:44:49 -0500

    use new URL

--- R/mangalPost.R
+++ R/mangalPost.R
@@ -10,14 +10,14 @@ mangalPost <- function(api, type, data)
   if(is.null(api$auth)) stop("You must be authenticated to post")
   if(is.null(data)) stop("Please provide data to add to the database")
   if(is.null(api[[type]]) | !("post" %in% api[[type]]$verbs)) stop(stringr::str_c("This API do not permit POSTing objects of type ",type))
-	if(!type == 'user') data$owner <- api$me
-	queryset <- httr::POST(stringr::str_c(api[[type]]$url, render_parameters(api)), body = jsonlite::toJSON(data, auto_unbox=TRUE), httr::add_headers("Content-type" = "application/json"))
-	if(httr::http_status(queryset)$category == "success")
-	{
-		new_entry <- httr::content(queryset)
-		return(new_entry)
-	} else {
-		print(httr::content(queryset))
-		stop(httr::http_status(queryset)$message)
-	}
+  if(!type == 'user') data$owner <- api$me
+  queryset <- httr::POST(stringr::str_c(api[[type]]$url, render_parameters(api)), body = jsonlite::toJSON(data, auto_unbox=TRUE), httr::add_headers("Content-type" = "application/json"))
+  if(httr::http_status(queryset)$category == "success")
+  {
+    new_entry <- httr::content(queryset)
+    return(new_entry)
+  } else {
+    print(httr::content(queryset))
+    stop(httr::http_status(queryset)$message)
+  }
 }
